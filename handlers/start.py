4b91323b43c316a95b9395e4cfcad5e4ddbae1fe
Ergashev Baxtiyor <86454228+Baxtiyor98@users.noreply.github.com>
2022-05-31 17:41:05 +0500

    add

--- handlers/start.py
+++ handlers/start.py
@@ -15,42 +15,42 @@ db = DBcommands()
 
 from loader import dp,bot
 
-@dp.message_handler(content_types=types.ContentType.PHOTO, state='*')
-async def image(message: types.Message):
-    if str(message.from_user.id) in ADMINS:
-        id = message.photo[0].file_id
-        await message.answer(text=id)
+# @dp.message_handler(content_types=types.ContentType.PHOTO, state='*')
+# async def image(message: types.Message):
+#     if str(message.from_user.id) in ADMINS:
+#         id = message.photo[0].file_id
+#         await message.answer(text=id)
 
 
 
-# @dp.message_handler(IsGroup(),commands=['start','lang'],state='*')
-# async def bot_start(message: types.Message,state:FSMContext):
-#     await state.finish()
-#     await bot.send_message(chat_id=GROUP[0],text="Бот не используется в группе, пожалуйста перейдите в чат!\n\nGuruhda bot ishlatilmaydi, iltimos, chatga o‘ting!")
+@dp.message_handler(IsGroup(),commands=['start','lang'],state='*')
+async def bot_start(message: types.Message,state:FSMContext):
+    await state.finish()
+    await bot.send_message(chat_id=GROUP[0],text="Бот не используется в группе, пожалуйста перейдите в чат!\n\nGuruhda bot ishlatilmaydi, iltimos, chatga o‘ting!")
 
-# @dp.message_handler(IsPrivate(),commands=['start','lang'],state='*')
-# async def bot_start(message: types.Message,state:FSMContext):
-#     await state.finish()
-#     await db.add_user()
-#     a = await message.answer('.',reply_markup=ReplyKeyboardRemove())
-#     await a.delete()
-#     await message.answer("Assalomu alaykum!\n\n🌐 <b>IT Park Tashkent</b>`ning rasmiy Telegram-botiga xush kelibsiz!\n📚Iltimos, ta’lim tilini tanlang.\n\n➖➖➖➖➖➖➖➖➖➖\n\nЗдраствуйте!\n\n🌐 Добро пожаловать на официальный Telegram-бот <b>IT Park Tashkent!</b>\n📚Пожалуйста, выберите язык обучения.",reply_markup=language)
-#     await Anketa.language.set()
+@dp.message_handler(IsPrivate(),commands=['start','lang'],state='*')
+async def bot_start(message: types.Message,state:FSMContext):
+    await state.finish()
+    await db.add_user()
+    a = await message.answer('.',reply_markup=ReplyKeyboardRemove())
+    await a.delete()
+    await message.answer("Assalomu alaykum!\n\n🌐 <b>IT Park Tashkent</b>`ning rasmiy Telegram-botiga xush kelibsiz!\n📚Iltimos, ta’lim tilini tanlang.\n\n➖➖➖➖➖➖➖➖➖➖\n\nЗдраствуйте!\n\n🌐 Добро пожаловать на официальный Telegram-бот <b>IT Park Tashkent!</b>\n📚Пожалуйста, выберите язык обучения.",reply_markup=language)
+    await Anketa.language.set()
 
-# @dp.callback_query_handler(lambda message: message.data in ['uz', 'ru'], state=Anketa.language)
-# async def set_language(call:types.CallbackQuery):
-#     await call.message.delete()
-#     a = await call.message.answer('.',reply_markup=ReplyKeyboardRemove())
-#     await a.delete()
-#     language = None
-#     if call.data == "uz":
-#         await db.set_language(call.data)
-#         language = 'uz'
-#         await call.message.answer("Iltimos, menyu orqali keyingi qadamni tanlang!", reply_markup=main_manu)
-#     elif call.data == "ru":
-#         await db.set_language(call.data)
-#         language = 'ru'
-#         await call.message.answer("Пожалуйста, выберите следующий шаг в разделе меню!", reply_markup=main_manuru)
-#     await call.answer(cache_time=0.02)
-#     await Anketa.main.set()
+@dp.callback_query_handler(lambda message: message.data in ['uz', 'ru'], state=Anketa.language)
+async def set_language(call:types.CallbackQuery):
+    await call.message.delete()
+    a = await call.message.answer('.',reply_markup=ReplyKeyboardRemove())
+    await a.delete()
+    language = None
+    if call.data == "uz":
+        await db.set_language(call.data)
+        language = 'uz'
+        await call.message.answer("Iltimos, menyu orqali keyingi qadamni tanlang!", reply_markup=main_manu)
+    elif call.data == "ru":
+        await db.set_language(call.data)
+        language = 'ru'
+        await call.message.answer("Пожалуйста, выберите следующий шаг в разделе меню!", reply_markup=main_manuru)
+    await call.answer(cache_time=0.02)
+    await Anketa.main.set()
 
